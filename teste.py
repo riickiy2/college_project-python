dfda86dea04091d382d0b82b25e0d2c65da20101
Ricardo Carmo <therick0156@gmail.com>
2026-03-20 12:32:32 -0300

    removing unnecessary folders

--- teste.py
+++
@@ -1,9 +0,0 @@
-while True:
-    num = int(input("Type aqui: "))
-    if num == 1:
-        print("Você é gay")
-    elif num == 2:
-        print("teu pai me deu ontem aquele safado")
-        break
-    else:
-        print("tente novamente")
